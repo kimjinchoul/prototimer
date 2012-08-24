ecfc17f6f628941b575ba59eb1d2b38c16ee05de
w.wilberforce.jr@gmail.com <w.wilberforce.jr@gmail.com@a2ffcec1-9213-8608-d0b1-6c125a1689a7>
2012-08-24 06:30:57 +0000

    添加关闭按钮，可以使用外部的配置文件来定义超时后的动作。可以判断系统是否处于锁定状态。这个是bzr-r15的版本

--- trunk/ProtoTimer/ProtoTimerDlg.h
+++ trunk/ProtoTimer/ProtoTimerDlg.h
@@ -27,6 +27,7 @@ protected:
 
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
+
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
@@ -34,27 +35,66 @@ public:
 	//CMonthCalCtrl m_calctrl;
 	//CEdit m_timevalue;
 	//CButton m_button;
-	int m_seconds_max;
+	const int SECONDS_MAX;
+	const int SECONDS_NOTIFY;
+
 	int m_seconds_cur;
+	int m_min_x0;
+	int m_min_0x;
+	int m_sec_x0;
+	int m_sec_0x;
+	
 	bool m_bstart;
-	afx_msg void OnBnClickedButton1();
-	afx_msg void OnTimer(UINT_PTR nID);
-	afx_msg HBRUSH OnCtlColor(CDC* pDC,
-		CWnd* pWnd,
-		UINT nCtlColor);
-	afx_msg BOOL OnMouseWheel(UINT nFlags,
-		short zDelta, CPoint pt);
+
+	//Draw
 	CBrush  m_whitebrush;
 	CStatic m_picbox_min_x0;
 	CStatic m_picbox_min_0x;
-	CBitmap m_bmp[10];
 	CStatic m_picbox_sec_x0;
 	CStatic m_picbox_sec_0x;
+
+	//Digit number pictures
+	CBitmap m_bmp[10];
+	CBitmap m_bmp_none;
+
+	//Taskbar notify
 	NOTIFYICONDATA m_notify_icon;
-	afx_msg LRESULT OnNotifyIcon(WPARAM wParam,LPARAM IParam);
-	afx_msg void OnSize(UINT nType, int cx, int cy);
+	bool m_benable_notify;
+
+	//Configure
+	CString m_config_mp3path;
+	CString m_config_cmdline;
+	CString m_config_argument;
+	CButton m_button_close;
+
+	//Start or stop timer.
+	void ChangeTimerStatus();
+	void UpdateDigitClock();
+
 	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nID);
+
+	//Let all transparent
+	afx_msg HBRUSH OnCtlColor(CDC* pDC,	CWnd* pWnd,	UINT nCtlColor);
+
+	//Reseverd
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+
+	//Mouse indicate
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
-	bool m_bminmal;
+	afx_msg void OnBnClickedButtonClose();
+
+	//System lock or unlock
+	afx_msg LRESULT OnSessionChange(WPARAM wParam, LPARAM lParam);
+
+	//Taskbar notify
+	afx_msg LRESULT OnNotifyIcon(WPARAM wParam,LPARAM IParam);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	void InitNotify();
+	void CheckNotify();
+	void FiniNotify();
+
+	//Timeout
+	void CheckTimout();
 };
